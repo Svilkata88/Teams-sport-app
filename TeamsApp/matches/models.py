296108bbe6e055c734bf62a3b81d47d57c4cbc85
Svilen Ivanov <134698218+Svilkata88@users.noreply.github.com>
2024-12-10 00:06:10 +0200

    add: 3 new models added - Matches, Referee, Playground

--- TeamsApp/matches/models.py
+++ TeamsApp/matches/models.py
@@ -1,14 +1,46 @@
 from django.db import models
+from players.models import Player
+from teams.models import Team
 
 
-class Match:
-    pass
+class Referee(models.Model):
+    name = models.CharField(max_length=50)
+    matches_count = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
 
 
-class Referee:
-    pass
+class Playground(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
-class PlayGround:
-    pass
+class Matches(models.Model):
+    class StatusChoices(models.TextChoices):
+        SCHEDULED = 'scheduled', 'Scheduled'
+        PLAYING = 'playing', 'Playing'
+        FINISHED = 'finished', 'Finished'
+        POSTPONED = 'postponed', 'Postponed'
+
+    home_team = models.ForeignKey(to=Team, on_delete=models.CASCADE, related_name='home_games')
+    home_team_score = models.SmallIntegerField(default=0)
+
+    away_team = models.ForeignKey(to=Team, on_delete=models.CASCADE, related_name='away_games')
+    away_team_score = models.SmallIntegerField(default=0)
+
+    referee = models.ForeignKey(to='matches.Referee', on_delete=models.CASCADE, related_name='matches', null=True, blank=True)
+    status = models.CharField(choices=StatusChoices.choices, default=StatusChoices.SCHEDULED)
+    playground = models.ForeignKey(to='matches.Playground', on_delete=models.CASCADE, related_name='pitch_matches')
+    date_added = models.DateField(auto_now_add=True, editable=False)
+    game_datetime = models.DateTimeField(auto_now_add=True)
+    creator = models.ForeignKey(to=Player, null=True, on_delete=models.SET_NULL, related_name='created_matches')
+
+    def __str__(self):
+        return f'{self.home_team} vs {self.away_team} | status: {self.status}'
+
+
 
