1bdd3503787c460e7f81e5ab48efea2adfb0c15d
Svilen Ivanov <134698218+Svilkata88@users.noreply.github.com>
2024-12-19 20:38:49 +0200

    add: added static

---
+++ static/js/updatePlayer.js
@@ -0,0 +1,12 @@
+let imgBox = document.getElementsByClassName('card-image');
+let img = imgBox[0].getElementsByTagName('img')[0];
+let inputImg = document.getElementById('playerImg');
+
+inputImg.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+
+    console.log(img)
+    let imageUrl;
+    imageUrl = URL.createObjectURL(file);
+    img.src = imageUrl;
+})
